466d4660e58e14c7f66ccca3d996414ae41a81ea
Easd99 <solanoae@uninorte.edu.co>
2021-02-28 16:43:30 -0500

    username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-          user_params.permit(:username, :email)
-        end
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+        user_params.permit(:username, :email)
       end
+  end
+  
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  end
 
-
-
+  
 end
+
+  
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.find_for_database_authentication(conditions={})
+    find_by(username: conditions[:email]) || find_by(email: conditions[:email])
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+         
 end
