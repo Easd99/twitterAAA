90ace135c66d5513e15f9203f46fc7d6d812cbc1
Easd99 <solanoae@uninorte.edu.co>
2021-02-28 12:09:01 -0500

    agregando users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,15 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user!
     def index
-    @tasks = Task.all 
+        @tasks = Task.all 
     end
+    def show
+
+    end
+
+    def new
+
+    end
+
 
 end
