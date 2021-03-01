ea907ee8d5c09bbad82bbb1002a1f5d744dd06fa
Sergio Montaño <sdmontano@uninorte.edu.co>
2021-02-28 23:35:38 -0500

    cambios 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,33 @@
 class TasksController < ApplicationController
     before_action :authenticate_user!
+
     def index
         @tasks = Task.all 
     end
-    def show
+
+
+    def new
+        @task = Task.new
 
     end
 
-    def new
-
+    def create
+        @task =Task.new(task_params)
+        if @task.save
+            redirect_to tasks_path, notice: "Enviado"
+        else
+            reder :new
+        end
+    end
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:description)
     end
 
 
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root  'home#index'
+  get '/index', to: 'tasks#index'
   resources :tasks
-  #get '/index', to: 'tasks#index'
 
-  
 end
