6c64b2e93e208c2f371dd6cbda73c3cbd7fa2c62
sdmontano <sdmontano@uninorte.edu.co>
2021-03-02 12:55:20 -0500

    herokuu

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.smtp_settings = {
-    address:'smtp.gmail.com',
-    port:'465',
-    domain:'gmail.com',
-    user_name: 'twitteraaaa@gmail.com',
-    password:  'nosequeponer123',
-    authentication: 'plain',
-    :ssl => true,
-    :tsl => true,
-    enable_starttls_auto: true
-}
 
 
 
