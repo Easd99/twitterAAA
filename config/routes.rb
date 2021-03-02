d941f168378b26f9ae9f2ad1f7f5288c8a0851f9
sdmontano <sdmontano@uninorte.edu.co>
2021-03-02 11:01:37 -0500

    Herokuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+Rails.application.routes.default_url_options[:host] = 'twitteraaa.herokuapp.com'
+
   devise_for :users
   get '/confirmationsuccessfull', to: 'home#successfull'
   root  'home#index'
