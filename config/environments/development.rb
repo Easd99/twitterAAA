bf6168282bcb2c780f56856e414bfac0db05f1d6
sdmontano <sdmontano@uninorte.edu.co>
2021-03-02 10:31:35 -0500

    changee

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = false
 
   #url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://stormy-thicket-73481.herokuapp.com', port: 3000 }
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -29,7 +29,6 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
@@ -38,8 +37,23 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+config.action_mailer.default_url_options = { :host => 'https://stormy-thicket-73481.herokuapp.com' }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+
+config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+  :authentication => :plain,
+  :address => "smtp.gmail.com",
+  :port => '587',
+  :domain => "gmail.com",
+  :user_name => 'servicetwitteraaaa@gmail.com',
+  :password => 'nosequeponer123'
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
