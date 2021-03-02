8d4068bd2770487d93235f7c761d39f805ca98a4
sdmontano <sdmontano@uninorte.edu.co>
2021-03-02 12:55:56 -0500

    herokuuuuu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,15 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.gmail.com",
+    :port => '587',
+    :domain => "gmail.com",
+    :user_name => 'servicetwitteraaaa@gmail.com',
+    :password => 'nosequeponer123'
+  }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
