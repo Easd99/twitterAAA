d3631006af5f9078b400b8c1471eff39272c6cac
sdmontano <sdmontano@uninorte.edu.co>
2021-03-26 15:09:32 -0500

    Cambios

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,20 @@ module Api
     module V1
         class ApiController < ActionController::Base
             skip_before_action :verify_authenticity_token
+            before_action :check_basic_auth
+
+            def check_basic_auth
+                #params[:email] paramas[:password]
+                authenticate_with_http_basic do |email, password|
+                    user = User.find_by_name(email)
+                    if !user.nil? && User.authenticate(email,password)
+                        @current_user = user  
+                    else
+                      head :unauthorized
+                    end  
+
+                end 
+            end    
         end
     end
 end
