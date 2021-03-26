f1bf523f51e7823e6d934cfa3615ddaed609b2ee
sdmontano <sdmontano@uninorte.edu.co>
2021-03-26 18:56:55 -0500

    Login con API

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :destroy]
+      resources :users, only: [:create, :index]
     end
   end
 
