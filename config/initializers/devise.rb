aeff098e39b8d71ee2cab9c7833a169ae4c7abcc
Easd99 <solanoae@uninorte.edu.co>
2021-03-30 22:09:05 -0500

    JWT 99.6

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,7 +310,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret =  Rails.application.credentials.jwt
+    jwt.secret =  Rails.application.secret_key_base
     jwt.dispatch_requests = [ ['POST', %r{^/authentication_tokens_login$}] ]
     jwt.revocation_requests = [ ['DELETE', %r{^/authentication_tokens_logout$}] ]
     #jwt.dispatch_requests = [ ['DELETE', %r{^/authentication_tokens$}] ]
