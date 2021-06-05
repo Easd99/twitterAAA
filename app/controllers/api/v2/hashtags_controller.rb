3f3c4374fd703486b858ed84a921e658d627d68e
Easd99 <solanoae@uninorte.edu.co>
2021-06-04 21:14:22 -0500

    ultimos cambios

--- app/controllers/api/v2/hashtags_controller.rb
+++ app/controllers/api/v2/hashtags_controller.rb
@@ -2,21 +2,19 @@ module Api
     module V2
         class HashtagsController < ApiController
 
-
-
             def index 
                 hashtag = "%#{setHashtag}%"
                 if setHashtag.match(/[#][a-zA-Z0-9]/)
                     tweets = Tweet.where("description LIKE ? ", hashtag)
                     tweetsList = []
                     tweets.each do |tweet|
-                        unless tweet.description.match(/#{setHashtag}[a-zA-Z]/) or tweet.description.match(/[a-zA-Z]#{setHashtag}/)
+                        unless tweet.description.match(/#{setHashtag}[a-zA-Z]/)
                             tweetsList.push(tweet)
                         end
                     end
                     render json: {tweets: tweetsList}
                 else
-                    
+                    render json: {error: 'error'}, status: 404
                 end
                 
             end
--- spec/controllers/api/v2/hashtags_controller_spec.rb
+++ spec/controllers/api/v2/hashtags_controller_spec.rb
@@ -21,5 +21,25 @@ RSpec.describe Api::V2::HashtagsController, "#index" do
         end
     end
 
+    context "When a hashtag is invalid" do
+        let(:user) {create(:user, :confirmed)}
+        let(:tweet) {create(:tweet, user: user)}
+        let(:tweet2) {create(:tweet, :invalid, user: user)}
+
+        before do
+
+            token = user.generate_jwt(user.jti)
+            request.headers["Authorization"] = "Bearer #{token}"
+            get :index, params: { hashtag: "hola"} 
+        end
+        it "should return HTTP success code" do
+            expect(response).to have_http_status(404)
+        end
+        it "should return Tweet in JSON body" do
+            json_response = JSON.parse(response.body)
+            expect(json_response.keys).to  match_array(["error"])
+        end
+    end    
+
     
 end    
